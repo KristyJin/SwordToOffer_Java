d12526b571b3c53dd6187137d03ac35f5eed6d81
Kristy <32897692+KristyJin@users.noreply.github.com>
2019-07-25 16:43:13 +0800

    Update No19.java

--- No19.java
+++ No19.java
@@ -14,18 +14,45 @@ package JianzhiOffer;
  */
 public class No19 {
     public static void main(String[] args) {
-
+        String s = "aaa";
+        String p = "aa.a";
+        System.out.println(isMatch(s,p));
     }
 
-    private static boolean match(char[] str, char[] pattern){
-        if(str.length <= 0 || pattern.length <= 0)
+    public static boolean isMatch(String s, String p) {
+        if (p.equals(".*")){
+            return true;
+        }
+        if (s == null || p == null){
+            return false;  // 如果字符串或者模式是空的，那么肯定没有匹配的
+        }
+        int strIndex = 0, patternIndex = 0;                 // 从字符串和模式的第一位开始进行匹配
+        return matchChar(s, strIndex, p, patternIndex);
+    }
+
+    public static boolean matchChar(String str, int strIndex, String pattern, int patternIndex){
+
+        if (strIndex == str.length() && patternIndex == pattern.length()){  // 字符串和模式同时都到末尾
+            return true;
+        }
+        if (strIndex != str.length() && patternIndex == pattern.length()){  // 模式先到末尾
             return false;
-        return matchCore(str,pattern);
-    }
-
-    private static boolean matchCore(char[] str, char[] pattern){
-
-
+        }
+        if (patternIndex + 1 < pattern.length() && pattern.charAt(patternIndex + 1) == '*'){
+            // 下一个字符是*
+            if ((strIndex != str.length() && str.charAt(strIndex) == pattern.charAt(patternIndex)) ||
+                    (strIndex != str.length() && pattern.charAt(patternIndex) == '.') ){
+                return matchChar(str, strIndex, pattern, patternIndex + 2) ||
+                        matchChar(str, strIndex + 1, pattern, patternIndex + 2) ||
+                        matchChar(str, strIndex + 1, pattern, patternIndex);
+            }else {
+                return matchChar(str, strIndex, pattern, patternIndex + 2);
+            }
+        }else if ((strIndex!=str.length() && str.charAt(strIndex) == pattern.charAt(patternIndex)) ||
+                (strIndex != str.length() && pattern.charAt(patternIndex) == '.')){
+            // 下一个字符不是*，字符串当前字符和模式中的当前字符相匹配
+            return matchChar(str, strIndex + 1, pattern, patternIndex + 1);
+        }
         return false;
     }
 
